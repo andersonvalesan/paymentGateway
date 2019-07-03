7ef366b94a61a8ff212b164343ee5a90268cab51
Anderson Valesan <andersonvalesan@hotmail.com>
2019-07-03 08:15:20 -0300

    create new route and include body-parser

--- apiRoutes/payments.js
+++ apiRoutes/payments.js
@@ -2,4 +2,10 @@ module.exports = function(app){
 	app.get('/payments', function(req, res){
 		res.send('OK');
 	});	
+
+	app.post('/payments/payment', function(req, res){
+		var payment = req.body;
+		console.log(payment);
+		res.send('OK');
+	});	
 }
