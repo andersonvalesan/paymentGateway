2d50c0b6048021fe08292558f5dbfe09367db525
Anderson Valesan <andersonvalesan@hotmail.com>
2019-07-06 00:02:27 -0300

    include express validator

--- apiRoutes/payments.js
+++ apiRoutes/payments.js
@@ -5,6 +5,25 @@ module.exports = function(app){
 
 	app.post('/payments/payment', function(req, res){
 		var payment = req.body;
+
+		req.assert("payment_type", "Forma de pagamento é obrigatória.")
+		.notEmpty();
+
+		req.assert("amount", "Valor é obrigatório e deve ser um decimal.")
+		.notEmpty()
+		.isFloat();
+
+		req.assert("currency", "Moeda é obrigatória e deve ter 3 caracteres")
+		.notEmpty()
+		.len(3,3);
+
+		var errors = req.validationErrors();
+
+		if (errors){
+		    console.log("Validation errors found");
+		    res.status(400).send(errors);
+		    return;
+		}
 		console.log('Creating payment...');
 
 		var connection = app.persistency.connectionFactory();
@@ -14,8 +33,12 @@ module.exports = function(app){
 		payment.date = new Date;
 
 	    paymentDao.salva(payment, function(exception, result){
-	      console.log('payment created: ' + result + exception);
-	      res.json(payment);
+	    	if(exception){
+	    		return res.status(400).send(exception);
+	    	}
+
+	    	console.log('payment created: ' + result);
+	    	return res.json(payment);
 	    });
 	});	
 }
